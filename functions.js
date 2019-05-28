f218e60674ed0974b2cf916b7e6cc5a746d3dcd9
ryanvanbaardwijk <ryan.baardwijk@mastek.com>
2019-05-28 16:24:13 +0100

    Finished function set

--- functions.js
+++ functions.js
@@ -31,31 +31,91 @@ function rollN(n) {
 
 
 // Test if an integer is even; return true if it is, false if not. E.g.
-
-
 // is_even(4) = true 
 // is_even(9) = false
-// Check if n is divisible by m. E.g.
+function isEven(n) {
+    if (Number.isInteger(n)) {
+        return n % 2 === 0;
+    }
+    else throw new TypeError;
+}
 
+// Check if n is divisible by m. E.g.
 // is_divisible(10,2) = true 
 // is_divisible(9,2) = false
-// Returns true if the user correctly guesses a secret random number between 1 and 10, false otherwise. E.g.
+function isDivisible(x,y) {
+    return x % y === 0;
+}
 
+// Returns true if the user correctly guesses a secret random number between 1 and 10, false otherwise. E.g.
 // secret = 6 
 // is_secret(4) = false 
 // is_secret(6) = true.
+function guessMeGen(n, y) {
+    y = y || Math.ceil(Math.random() * n);
+    return (x) => {
+        if (0 <= x <= y){
+            return y === x;
+        } 
+        else throw new RangeError;
+    };
+};
+
+
 // Write a function that returns an array containing the odd numbers in a given array. E.g. odds([3,4,5,4,3,8,10,11]) = [3,5,3,11].
+function odds(values) {
+    return values.filter((x) => !isEven(x));
+}
+
 
 // Write a function that sums the elements of an array. E.g. sum([3,4,6]) = 13. [Hint: reduce might be useful here.]
+function sum(values){ 
+    return values.reduce((acc, x) => acc += x, 0);
+}
 
 // Write a function that finds the mean of the elements of an array. E.g. mean([3,4,8]) = 5.
+function mean(values){
+    const sumValues = sum(values);
+    return sumValues / values.length;
+}
 
 // Returns true if string is palindrome, false otherwise. E.g.
-
 // is_palindrome("anutforajaroftuna") = true 
 // is_palindrome("fanta") = false
+function isPalindrome(s) {
+    let l = (isEven(s.length) ? s.length/2 : (s.length-1)/2 );
+    for (let i = 0; i < l; i++){
+        if (s[i] != s[s.length-i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 // Returns a list of the factors of a given integer. E.g. factors(10) = [1,2,5,10].
+function factorise(n){
+    let factors = []
+    for (let i = 0; i < n/2; i++){
+        if (n%i===0) factors.push(i, n/i);
+    }
+    return factors;
+
+}
 
 // Write a function that takes a string as input and returns a string containing its vowels. E.g. vowels("delicious") = "eiiou".
+function vowels(s){
+    let output = ""
+    const vowels = ["aeiou"];
+    for (let j = 0; j < n/2; j++){
+        for (let i = 0; i < n/2; i++){
+            if (s[j] === vowels[i]){
+                output += s[j];
+                break;
+            }
+        }
+    }
+    return vowels
 
-module.exports = {double , triple, first, coinFlip, roll, rollN};
+}
+
+module.exports = {double , triple, first, coinFlip, roll, rollN, isEven, isDivisible, guessMeGen};
--- functions.test.js
+++ functions.test.js
@@ -41,81 +41,148 @@ toBeWithinRange(received, floor, ceiling) {
 });
 
 
-// Double a number. E.g. double(2) = 4.
-test('Duobbles 2 to equal 4', () => {
-  expect(myFuncs.double(2)).toBe(4);
+describe("Double a number. E.g. double(2) = 4.", () => {
+    test('Duobbles 2 to equal 4', () => {
+    expect(myFuncs.double(2)).toBe(4);
+    });
+
+    test('Doubble -2 to equal -4', () => {
+        expect(myFuncs.double(-2)).toBe(-4);
+    });
+
+    test('Doubble 8 to equal 16', () => {
+        expect(myFuncs.double(8)).toBe(16);
+    });
 });
 
-test('Doubble -2 to equal -4', () => {
-    expect(myFuncs.double(-2)).toBe(-4);
+describe("Triple a number. E.g. triple(3) = 9.", () => {
+    test('Triples 2 to equal 6', () => {
+        expect(myFuncs.triple(2)).toBe(6);
+    });
+
+    test('Triples -2 to equal -6', () => {
+        expect(myFuncs.triple(-2)).toBe(-6);
+    });
+
+    test('Triples 1 to equal 3', () => {
+        expect(myFuncs.triple(1)).toBe(3);
+    });
 });
 
-test('Doubble 8 to equal 16', () => {
-    expect(myFuncs.double(8)).toBe(16);
+describe("Return the first character of a string. E.g. first('cat') = 'c'.", () => {
+    test('first("cat") = "c"', () => {
+        expect(myFuncs.first("cat")).toBe("c");
+    });
+
+    test('first("Dog") = "D"', () => {
+        expect(myFuncs.first("Dog")).toBe("D");
+    });
+
+    test('first("1234") = "1"', () => {
+        expect(myFuncs.first("1234")).toBe("1");
+    });
+
+    test('first(" 156") = " "', () => {
+        expect(myFuncs.first(" 156")).toBe(" ");
+    });
 });
 
-// Triple a number. E.g. triple(3) = 9.
-test('Triples 2 to equal 6', () => {
-    expect(myFuncs.triple(2)).toBe(6);
-});
-
-test('Triples -2 to equal -6', () => {
-    expect(myFuncs.triple(-2)).toBe(-6);
-});
-
-test('Triples 1 to equal 3', () => {
-    expect(myFuncs.triple(1)).toBe(3);
-});
-
-// Return the first character of a string. E.g. first("cat") = "c".
-test('first("cat") = "c"', () => {
-    expect(myFuncs.first("cat")).toBe("c");
-});
-
-test('first("Dog") = "D"', () => {
-    expect(myFuncs.first("Dog")).toBe("D");
-});
-
-test('first("1234") = "1"', () => {
-    expect(myFuncs.first("1234")).toBe("1");
-});
-
-test('first(" 156") = " "', () => {
-    expect(myFuncs.first(" 156")).toBe(" ");
-});
-
-// Write a toin cossing function. E.g. toss() = "heads".
-test('coinToss() = "heads" or "tails"', () => {
-    const headsOrTails = {
-        asymmetricMatch: actual => actual === 'heads' || actual === 'tails'
-    };
-    for (let i = 0; i < 1000; i++) {
-        expect(myFuncs.coinFlip()).toEqual(headsOrTails);
-    }
-});
-
-// Write a dice rolling function. E.g. dice() = 4.
-test('roll() = 1 or 2 or 3 or 4 or 5 or 6', () => {
-    for (let i = 0; i < 1000; i++) {
-        expect(myFuncs.roll()).toBeIntWithinRange(1,6);
-    };
-});
-
-// Write a dice rolling function, where the dice can have any number of sides. E.g. dice(10)=9.
-[6,100,1000].forEach(testData => {
-    test(`rollN(${testData}) = 1 to ${testData}`, () => {
-        let val;
-        let values = [];
-        let stanDiv  = testData * 0.05;
-        for (let i = 0; i < 100000; i++) {
-            val = myFuncs.rollN(testData);
-            values.push(val);
-            expect(val).toBeIntWithinRange(1,testData);
+describe("Write a toin cossing function. E.g. toss() = 'heads'.", () => {
+    test('coinToss() = "heads" or "tails"', () => {
+        const headsOrTails = {
+            asymmetricMatch: actual => actual === 'heads' || actual === 'tails'
         };
-        expect(Math.max(...values)).toEqual(testData);
-        expect(Math.min(...values)).toEqual(1);
-        const sum = values.reduce((acc, x) => acc += x, 0);
-        const ave = sum / values.length;
-        expect(ave).toBeWithinRange(testData/2-stanDiv, testData/2+stanDiv);
+        for (let i = 0; i < 1000; i++) {
+            expect(myFuncs.coinFlip()).toEqual(headsOrTails);
+        }
+    });
+});
+
+describe("Write a dice rolling function. E.g. dice() = 4.", () => {
+    test('roll() = 1 or 2 or 3 or 4 or 5 or 6', () => {
+        for (let i = 0; i < 1000; i++) {
+            expect(myFuncs.roll()).toBeIntWithinRange(1,6);
+        };
+    });
+});
+
+describe("Write a dice rolling function, where the dice can have any number of sides. E.g. dice(10)=9.", () => {
+    [6,100,1000].forEach(testData => {
+        test(`rollN(${testData}) = 1 to ${testData}`, () => {
+            let val;
+            let values = [];
+            let stanDiv  = testData * 0.15;
+            for (let i = 0; i < 100000; i++) {
+                val = myFuncs.rollN(testData);
+                values.push(val);
+                expect(val).toBeIntWithinRange(1,testData);
+            };
+        
+        
+            expect(Math.max(...values)).toEqual(testData);
+            expect(Math.min(...values)).toEqual(1);
+            const sum = values.reduce((acc, x) => acc += x, 0);
+            const ave = sum / values.length;
+            expect(ave).toBeWithinRange(testData/2-stanDiv, testData/2+stanDiv);
+        });
+    });
+});
+
+describe("Test if an integer is even; return true if it is, false if not. E.g.", () => {
+    test('isEven(4) = true', () => {
+        expect(myFuncs.isEven(2)).toEqual(true);
+    });
+
+    test('isEven(9) = false', () => {
+        expect(myFuncs.isEven(3)).toEqual(false);
+    });
+
+    test('isEven(100) = true', () => {
+        expect(myFuncs.isEven(100)).toEqual(true);
+    });
+
+    test('isEven(100.0) = true', () => {
+        expect(myFuncs.isEven(100.0)).toEqual(true);
+    });
+
+    test('isEven(101.1) throws TypeError', () => {
+        expect(() => myFuncs.isEven(101.1)).toThrow(TypeError);
+    });
+});
+
+describe("Check if n is divisible by m. E.g.", () => {
+    test('is_divisible(10,2) = true', () => {
+        expect(myFuncs.isDivisible(10,2)).toEqual(true);
+    });
+
+    test('is_divisible(9,2) = false', () => {
+        expect(myFuncs.isDivisible(9,2)).toEqual(false);
+    });
+});
+
+describe("Returns true if the user correctly guesses a secret random number between 1 and 10, false otherwise. E.g.", () => {
+    let game;
+    beforeAll(() => {
+        game = myFuncs.guessMeGen(10, 5)
+    });
+
+    test('test the game con only be won with one value', () => {
+        expect(game).toBe(Function);
+    });
+
+    test('test the game con only be won with one value', () => {
+        expect(game(1)).toEqual(false);
+    });
+
+    test('test the game con only be won with one value', () => {
+        expect(game(5)).toEqual(true);
+    });
+
+    test('test the game con only be won with one value', () => {
+        expect(game(-1)).toThrow(RangeError);
+    });
+
+    test('test the game con only be won with one value', () => {
+        expect(game(11)).toThrow(RangeError);
     });
 });
